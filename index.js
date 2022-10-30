5b6ce3e65547e40dc3293d78486de2671f0e59de
aungheinbo07 <glober912@gmail.com>
2022-10-30 10:52:51 +0630

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+myName = "aung hein bo";
+
+export function showUI(){
+    let h1 = document.createElement("h1");
+    h1.innerText = myName;
+    h1.style.position = "fixed";
+    h1.style.zIndex = 1000;
+    h1.style.top = 0;
+    h1.style.left = 0;
+    h1.style.right = 0;
+    h1.style.bottom = 0;
+    h1.style.margin = "auto";
+    document.body.append(h1);
+}
